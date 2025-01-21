2db06c2099a02779547bd437741355ecde9b1672
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2025-01-21 16:26:10 -0300

    feat(): cria mascara de estudo

--- aula01.c
+++ aula01.c
@@ -3,7 +3,9 @@
 int main(void){
     int numero = 10;
 
+    // Função de saída de dados 'printf':
     printf("O numero digitado foi: %d \n", numero);
+    // '%d' é uma máscara/marcador que remete a um tipo de dado e, neste caso, um dado inteiro (%d), afim de especificar que tipo de dado a função de saída de dados 'printf' irá trazer, se for outro tipo de váriavel você utilizar %d, dará um erro!
 
     return 0;
 }
